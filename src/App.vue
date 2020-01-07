350eb305bd480202993eeade95eba8b39b1e4104
wangzhaojun <wangzhaojun1@jd.com>
2020-01-07 11:07:20 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <wzj-marquee :content="content"></wzj-marquee>
+  </div>
+</template>
+
+<script>
+import WzjMarquee from './components/WzjMarquee'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      content: '跑马灯文字跑马灯文字跑马灯文字跑马灯文字跑马灯文字跑马灯文字跑马灯文字'
+    };
+  },
+  components: {
+    WzjMarquee
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
